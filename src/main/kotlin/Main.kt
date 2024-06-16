baba5b555875f282a48f043518403554cf09c107
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-16 19:21:16 +0300

    add filling board with players

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,7 @@ class ConnectFour {
     private var rows = 6
     private var columns = 7
     private var players = mutableMapOf<Int, Player>()
-    private var board = List(rows) { List(columns) { " " }  }
+    private var board: MutableList<MutableList<Char>>
 
     init {
         println("Connect Four")
@@ -15,31 +15,34 @@ class ConnectFour {
         setBoardSize()
         println("${players[0]?.name} VS ${players[1]?.name}")
         println("$rows X $columns board")
+        board = MutableList(columns) { MutableList(rows) { ' ' } }
+        printBoard()
     }
     private fun setBoardSize() {
+        var sizes: List<Int>
         while (true) {
             println("Set the board dimensions (Rows x Columns)")
             println("Press Enter for default (6 x 7)")
             val input = readln()
-
-            try {
-                if (input.isEmpty()) { rows = 6; columns = 7; break } // by default
+            sizes = try {
+                if (input.isEmpty()) listOf(6, 7)  // by default
                 else input.trim().split(Regex("\\s*[Xx]\\s*")).map(String::toInt)
             } catch (e: Exception) {
                 println("Invalid input"); continue
             }
             when {
-                rows !in 5..9 -> println("Board rows should be from 5 to 9")
-                columns !in 5..9 -> println("Board columns should be from 5 to 9")
+                sizes[0] !in 5..9 -> println("Board rows should be from 5 to 9")
+                sizes[1] !in 5..9 -> println("Board columns should be from 5 to 9")
                 else -> break
             }
         }
+        rows = sizes[0]; columns = sizes[1]
     }
-    fun printBoard() {
+    private fun printBoard() {
         var col = 1
         repeat(columns) { print(" ${col++}") }; println()
         var row = 0; col = 0
-        repeat(rows) { print("║"); repeat(columns) { print("${board[row][col++]}║") }; println(); row++; col = 0 }
+        repeat(rows) { print("║"); repeat(columns) { print("${board[col++][row]}║") }; println(); row++; col = 0 }
         print("╚"); repeat(columns - 1) { print("═╩") }; println("═╝")
     }
     private fun setPlayers() {
@@ -50,20 +53,35 @@ class ConnectFour {
     }
     fun makeMove() {
         var turnNum = 0
-        var turn: String // means column where will put '0' or '*'
+        var input: String // means column where will put 'o' or '*'
+        var column: Int
+        var row: Int
         while (true) {
+
+            println("${players[turnNum % 2]?.name}'s turn:")
+            input = readln()
+            if (input == "end") { println("Game over!"); return }
+            try {
+                column = input.toInt()
+                if (column !in 1..columns) {
+                    println("The column number is out of range (1 - $columns)")
+                    continue
+                }
+            } catch (e: Exception) {
+                println("Incorrect column number")
+                continue
+            }
+            column--
+            if (' ' !in board[column]) { println("Column ${++column} is full"); continue }
+            row = board[column].indexOfLast { it == ' ' }
+            board[column][row] = players[turnNum++ % 2]?.sign!!
             printBoard()
-            println("${players[turnNum++ % 2]?.name}'s turn:")
-            turn = readln()
-            if (turn == "end") { println("Game over!"); return }
         }
     }
 }
 
 
-
 fun main() {
     val connectFour = ConnectFour()
     connectFour.makeMove()
-    connectFour.printBoard()
 }
