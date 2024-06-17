2ce45909f1cc5c381c0ba73eb2e403301b306ddb
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-17 18:25:40 +0300

    add options multiple games with scores

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 package connectfour
 
-class Player(val name: String, val sign: Char)
+class Player(val name: String, val sign: Char, var score: Int = 0)
 
 class ConnectFour {
     private var rows = 6
@@ -8,15 +8,60 @@ class ConnectFour {
     private var players = mutableMapOf<Int, Player>()
     private var board = mutableListOf<MutableList<Char>>()
     private var col = 0 // current column in turn
+    private lateinit var curPlayer: Player
+    private var turnNum = 0
+    private var cntGames = 1
 
     init {
         println("Connect Four")
         setPlayers()
         setBoardSize()
+        setCountGames()
         println("${players[0]?.name} VS ${players[1]?.name}")
         println("$rows X $columns board")
-        board = MutableList(columns) { MutableList(rows) { ' ' } }
-        printBoard()
+        println(if (cntGames == 1) "Single game" else "Total $cntGames games")
+    }
+
+    fun menu() {
+        var result: String
+        for (gameNum in 1..cntGames) {
+            if (cntGames > 1) println("Game #$gameNum")
+            createEmptyBoard()
+            printBoard()
+            result = game()
+            when (result) {
+                "end" -> break
+                "draw" -> println("It is a draw")
+                else -> println("Player $result won")
+            }
+            if (cntGames > 1) {
+                println("""
+                Score
+                ${players[0]?.name}: ${players[0]?.score} ${players[1]?.name}: ${players[1]?.score}
+            """.trimIndent())
+            }
+        }
+        println("Game over!")
+    }
+    private fun setCountGames() {
+        var input: String
+        while (true) {
+            println("Do you want to play single or multiple games?\n" +
+                    "For a single game, input 1 or press Enter\n" +
+                    "Input a number of games:")
+            input = readln()
+            cntGames = try {
+                when {
+                    input.isBlank() -> 1
+                    input.toInt() < 1 -> throw Exception()
+                    else -> input.toInt()
+                }
+            } catch (e: Exception) {
+                println("Invalid input"); continue
+            }
+            break
+        }
+
     }
     private fun setBoardSize() {
         var sizes: List<Int>
@@ -37,6 +82,9 @@ class ConnectFour {
             }
         }
         rows = sizes[0]; columns = sizes[1]
+    }
+    private fun createEmptyBoard() {
+        board = MutableList(columns) { MutableList(rows) { ' ' } }
     }
     private fun printBoard() {
         var col = 1
@@ -65,13 +113,29 @@ class ConnectFour {
     }
     private fun notInColumnRange() = col !in 0 until columns
     private fun columnIsFull() = ' ' !in board[col]
+    private fun result(): String {
+        val boardToString: String = buildString { board.forEach { c -> append(c.joinToString("") + "n") } } // n == \n
+        // for checking with Regex
 
-    fun makeMove(): String {
-        var turnNum = 0
+        if (boardToString.count { it == ' '} == 0) {
+            players.forEach {it.value.score++} // +1 for each player
+            return "draw"
+        }
+        val regexPattern = buildString {
+            append(".*([${curPlayer.sign}]{4}|")
+            append("([${curPlayer.sign}].{$rows}){3}[${curPlayer.sign}]|")
+            append("([${curPlayer.sign}].{${rows - 1}}){3}[${curPlayer.sign}]|")
+            append("([${curPlayer.sign}].{${rows + 1}}){3}[${curPlayer.sign}]).*")
+        }
+        if (Regex(regexPattern).matches(boardToString)) {
+            curPlayer.score += 2
+            return "win"
+        }
+        return "next turn"
+    }
+    private fun game(): String {
         var input: String // for checking input data
         var row: Int
-        var curPlayer: Player
-        var boardToString: String // for checking with Regex
         while (true) {
             curPlayer = players[turnNum % 2]!! // set player
             println("${curPlayer.name}'s turn:")
@@ -81,28 +145,18 @@ class ConnectFour {
 
             row = board[col].indexOfLast { it == ' ' }
             board[col][row] = curPlayer.sign // set sign of current player in empty place
-            turnNum++ // set next player
-            printBoard()
 
-            boardToString = buildString { board.forEach { c -> append(c.joinToString("") + "n") } } // n == \n
-            if (boardToString.count { it == ' '} == 0) return "draw"
-            val regexPattern = buildString {
-                append(".*([${curPlayer.sign}]{4}|")
-                append("([${curPlayer.sign}].{$rows}){3}[${curPlayer.sign}]|")
-                append("([${curPlayer.sign}].{${rows - 1}}){3}[${curPlayer.sign}]|")
-                append("([${curPlayer.sign}].{${rows + 1}}){3}[${curPlayer.sign}]).*")
+            printBoard()
+            turnNum++
+            when (result()) {
+                "draw" -> return "draw"
+                "win" -> return curPlayer.name
             }
-            if (Regex(regexPattern).matches(boardToString)) return curPlayer.name
         }
     }
 }
 
 fun main() {
     val connectFour = ConnectFour()
-    val result = connectFour.makeMove()
-    println( when (result) {
-        "end" -> ""
-        "draw" -> "It is a draw"
-        else -> "Player $result won"
-    } + "\nGame over!")
+    connectFour.menu()
 }
